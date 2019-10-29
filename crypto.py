c567b519118218f9a4128c07caab0358e4f3a995
100102324 <hopdeww@gmail.co>
2019-10-29 08:48:00 -0500

    did a lot- added mapper and created a cipher for the Strings and basic cartography or whatever

--- crypto.py
+++ crypto.py
@@ -5,3 +5,19 @@
 
 # so now we want to write a python method to made the computer do this
 #   Transcription Cipher
+
+# original: this_is_a_secret_that_i_want_to_transmit
+# encrypted:
+
+def scramble2Encrypt(plainText):
+    evenChars = ""
+    oddChars = ""
+    charCount = 0
+    for ch in plainText:
+        if charCount % 2 == 0:
+            evenChars = evenChars
+        else:
+            oddChars = oddChars + ch
+        charCount = charCount + 1
+    cipherText = oddChars + evenChars
+    return cipherText
