402cf954ce336dd023166520dcc759243a128ea6
100102324 <hopdeww@gmail.co>
2019-10-29 08:37:31 -0500

    changed the name and added the purpose of this file

---
+++ crypto.py
@@ -0,0 +1,7 @@
+# this_is_a_secret_that_i_want_to_transmit
+
+# tis _ asce _ esg _ htiwn _ otasi
+# hsi_ertmsaeta_att_rnmt
+
+# so now we want to write a python method to made the computer do this
+#   Transcription Cipher
--- transposition.py
+++
@@ -1,4 +0,0 @@
-# this_is_a_secret_that_i_want_to_transmit
-
-# t i s _ a s c e _ e s g _ h t i w n _ o t a s i
-# h s i __ e r t m s a e t a _ _ a t t _ r n m t
